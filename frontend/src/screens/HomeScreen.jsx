98d5231ded4c470027adb5def7532f6ecba41708
Kevin Leister <kevinleister07@gmail.com>
2023-11-16 12:27:43 -0600

    List Products - create product component, display on HomeScreen

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Row, Col, Container } from 'react-bootstrap'
 import products from '../products'
+import Product from '../components/Product'
 
 const HomeScreen = () => {
   return (
@@ -8,8 +9,8 @@ const HomeScreen = () => {
       <h1>Latest Products</h1>
         <Row>
             {products.map(product => (
-                <Col sm={12} md={6} lg={4} xl={3}>
-                    <h3>{product.name}</h3>
+                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                    <Product product={product} />
                 </Col>
             ))}
         </Row>
