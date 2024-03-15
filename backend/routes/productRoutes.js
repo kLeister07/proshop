215fca8bfda37dcac352005d0d7100e03ea554f7
Kevin Leister <kevinleister07@gmail.com>
2024-03-15 15:57:08 -0500

    setup mongodb, mongoose, and err middleware

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,24 @@
+import express from "express";
+const router = express.Router();
+import asyncHandler from "../middleware/asyncHandler.js";
+import Product from "../models/productModel.js";
+
+
+router.get("/", asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+  }));
+  
+router.get("/:id", asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    console.log(product);
+    if (product) {
+     return res.json(product);
+    } else {
+      res.status(404);
+      throw new Error("Resource not found");
+    }
+
+  }));
+
+export default router;
