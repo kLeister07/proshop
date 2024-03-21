63a41e653eeaee7e4dd7f08d36214c3c4b6e510b
Kevin Leister <kevinleister07@gmail.com>
2024-03-20 23:07:32 -0500

    add user email and password validation

--- backend/data/users.js
+++ backend/data/users.js
@@ -4,19 +4,19 @@ import e from 'express';
 const users = [
     {
         name: 'Admin User',
-        email: 'admin@email,com',
+        email: 'admin@email.com',
         password: bcrypt.hashSync('123456', 10),
         isAdmin: true,
     },
     {
         name: 'John Doe',
-        email: 'john@email,com',
+        email: 'john@email.com',
         password: bcrypt.hashSync('123456', 10),
         isAdmin: false,
     },
     {
         name: 'Jane Doe',
-        email: 'jane@email,com',
+        email: 'jane@email.com',
         password: bcrypt.hashSync('123456', 10),
         isAdmin: false,
     },
