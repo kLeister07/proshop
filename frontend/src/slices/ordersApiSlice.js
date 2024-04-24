edac5d888019c2cef4d4dc90955c0efda3584714
Kevin Leister <kevinleister07@gmail.com>
2024-04-23 19:59:16 -0500

    List Orders For Admin

--- frontend/src/slices/ordersApiSlice.js
+++ frontend/src/slices/ordersApiSlice.js
@@ -36,6 +36,12 @@ export const ordersApiSlice = apiSlice.injectEndpoints({
       }),
       keepUnusedDataFor: 5,
     }),
+    getOrders: builder.query({
+      query: () => ({
+        url: ORDERS_URL,
+      }),
+    }),
+    keepUnusedDataFor: 5
   }),
 });
 
@@ -45,4 +51,5 @@ export const {
   usePayOrderMutation,
   useGetPaypalClientIdQuery,
   useGetMyOrdersQuery,
+  useGetOrdersQuery,
 } = ordersApiSlice;
