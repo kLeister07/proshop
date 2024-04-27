17dc66857d1f13b44285a7905cbf06baa9447231
Kevin Leister <kevinleister07@gmail.com>
2024-04-27 17:51:19 -0500

    Edit Product

--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -8,6 +8,7 @@ export const productsApiSlice = apiSlice.injectEndpoints({
                 url: PRODUCTS_URL,
             }),
             keepUnusedDataFor: 5,
+            providesTags: ["Products"],
         }),
         getProductDetails: builder.query({
             query: (productId) => ({
@@ -22,7 +23,15 @@ export const productsApiSlice = apiSlice.injectEndpoints({
             }),
         invalidatesTags: ["Product"],
         }),
+        updateProduct: builder.mutation({
+            query: (data) => ({
+                url: `${PRODUCTS_URL}/${data.productId}`,
+                method: "PUT",
+                body: data,
+            }),
+            invalidatesTags: ["Products"], 
+        }),
     }),
 });
 
-export const { useGetProductsQuery, useGetProductDetailsQuery, useCreateProductMutation } = productsApiSlice;
+export const { useGetProductsQuery, useGetProductDetailsQuery, useCreateProductMutation, useUpdateProductMutation } = productsApiSlice;
