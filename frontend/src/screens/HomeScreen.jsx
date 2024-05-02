300b3c41630170c6c89dcad6f18b72325f09af1a
Kevin Leister <kevinleister07@gmail.com>
2024-05-02 17:07:00 -0500

    Paginate Products

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,33 +1,38 @@
-import React from 'react'
-import { Row, Col } from 'react-bootstrap'
-import Product from '../components/Product'
-import { useGetProductsQuery } from '../slices/productsApiSlice'
-import Loader from '../components/Loader'
-import Message from '../components/Message'
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { Row, Col } from 'react-bootstrap';
+import Product from '../components/Product';
+import { useGetProductsQuery } from '../slices/productsApiSlice';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 
 const HomeScreen = () => {
-const { data: products, isLoading, error } = useGetProductsQuery();
+  const { pageNumber } = useParams();
+  
+  const { data, isLoading, error } = useGetProductsQuery({ pageNumber});
 
   return (
     <>
-    {isLoading ? (
+      {isLoading ? (
         <Loader />
-    ) : error ? (
-    <Message variant='danger'>{ error?.data?.message || error.error }</Message>
-    ) : (
-    <>       
-    <h1>Latest Products</h1>
-        <Row>
-            {products.map(product => (
-                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                    <Product product={product} />
-                </Col>
+      ) : error ? (
+        <Message variant='danger'>
+          {error?.data?.message || error.error}
+        </Message>
+      ) : (
+        <>
+          <h1>Latest Products</h1>
+          <Row>
+            {data.products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
             ))}
-            </Row> 
-            </>) }
-
+          </Row>
+        </>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
