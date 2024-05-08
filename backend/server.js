f30473d617fa838820b6d6e2688fce52fabfe216
Kevin Leister <kevinleister07@gmail.com>
2024-05-08 16:09:28 -0500

    Prepare For Production

--- backend/server.js
+++ backend/server.js
@@ -23,10 +23,6 @@ app.use(express.urlencoded({ extended: true }));
 // Cookie parser middleware
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
-
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
@@ -38,6 +34,20 @@ app.get('/api/config/paypal', (req, res) =>
 
 const __dirname = path.resolve(); // Set __dirname to the absolute path of the current directory
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if(process.env.NODE_ENV === 'production') {
+  // Set frontend build folder as static folder
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  // any route that is not an API route, will be redirected to the index.html file in the frontend build folder
+  app.get('*', (req, res) => res.sendFile
+    (path.resolve(__dirname, 'frontend', 'build', 'index.html')));
+} else {
+  // If not in production, display a message that the API is running
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
